8b849032d48db76f9a6e0ac3ed0e86817713aa0d
sakumatto <6534019+sakumatto@users.noreply.github.com>
2020-10-25 09:57:48 +0200

    Update index.js

--- src/pages/blog/index.js
+++ src/pages/blog/index.js
@@ -22,7 +22,7 @@ export default class BlogIndexPage extends React.Component {
               padding: '1rem',
             }}
           >
-            Latest Stories
+            Kirjoituksia puulämmitteisistä tulisijoista
           </h1>
         </div>
         <section className="section">
