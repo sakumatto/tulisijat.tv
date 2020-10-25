0421f5cc0fbeb8513545184f1c2cae512f58eb0d
sakumatto <6534019+sakumatto@users.noreply.github.com>
2020-10-25 09:04:53 +0200

    Update Navbar.js

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -42,7 +42,7 @@ const Navbar = class extends React.Component {
         <div className="container">
           <div className="navbar-brand">
             <Link to="/" className="navbar-item" title="Logo">
-              <img src={logo} alt="Kaldi" style={{ width: '88px' }} />
+              <img src={logo} alt="tulisijat.tv" style={{ width: '88px' }} />
             </Link>
             {/* Hamburger menu */}
             <div
@@ -61,32 +61,17 @@ const Navbar = class extends React.Component {
           >
             <div className="navbar-start has-text-centered">
               <Link className="navbar-item" to="/about">
-                About
+                Taustoja
               </Link>
               <Link className="navbar-item" to="/products">
-                Products
+                Tulisijan osat
               </Link>
               <Link className="navbar-item" to="/blog">
-                Blog
+                Kirjoituksia
               </Link>
               <Link className="navbar-item" to="/contact">
-                Contact
+                Yhteydenotto
               </Link>
-              <Link className="navbar-item" to="/contact/examples">
-                Form Examples
-              </Link>
-            </div>
-            <div className="navbar-end has-text-centered">
-              <a
-                className="navbar-item"
-                href="https://github.com/netlify-templates/gatsby-starter-netlify-cms"
-                target="_blank"
-                rel="noopener noreferrer"
-              >
-                <span className="icon">
-                  <img src={github} alt="Github" />
-                </span>
-              </a>
             </div>
           </div>
         </div>
