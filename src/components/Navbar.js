9e5795b1f43741e05d5f6da05d3ee2c2f898561e
sakumatto <6534019+sakumatto@users.noreply.github.com>
2020-10-30 18:37:52 +0200

    Update Navbar.js
    Logoksi tulisijat-tv-logo.jpg

--- src/components/Navbar.js
+++ src/components/Navbar.js
@@ -1,7 +1,7 @@
 import React from 'react'
 import { Link } from 'gatsby'
 import github from '../img/github-icon.svg'
-import logo from '../img/logo.svg'
+import logo from '../img/tulisijat-tv-logo.jpg'
 
 const Navbar = class extends React.Component {
   constructor(props) {
